b9a118898b323bb7b183296667c9f5802cf02234
teetawat <teetawat.tch@gmail.com>
2022-01-30 00:11:15 +0700

    fixed api fail to fetch data on js

--- src/App.js
+++ src/App.js
@@ -23,32 +23,32 @@ const SubTitle = () => {
   return <h3>แยกตามจังหวัด</h3>;
 };
 const Total = () => {
-  const [totalData, setTotal] = useState({ totalDataNew: 0 });
-  const [totalDataD, setTotalD] = useState({ totalDataRec: 0 });
-  const [totalDataR, setTotalR] = useState({ totalDataDet: 0 });
+  const [totalData, setTotal] = useState([]);
+  const [totalDataD, setTotalD] = useState([]);
+  const [totalDataR, setTotalR] = useState([]);
 
   function getTotal() {
     fetch("https://covid19.ddc.moph.go.th/api/Cases/timeline-cases-all")
       .then((res) => res.json())
-      .then((data) => setTotal({ totalDataNew: data[-1].total_case }));
+      .then((data) => setTotal(data[data.length -1 ].total_case));
   }
   function getTotalD() {
     fetch("https://covid19.ddc.moph.go.th/api/Cases/timeline-cases-all")
       .then((res) => res.json())
-      .then((data) => setTotalD({ totalDataDet: data[-1].total_death }));
+      .then((data) => setTotalD(data[data.length -1 ].total_death));
   }
   function getTotalR() {
     fetch("https://covid19.ddc.moph.go.th/api/Cases/timeline-cases-all")
       .then((res) => res.json())
-      .then((data) => setTotalR({ totalDataRec: data[-1].total_recovered }));
+      .then((data) => setTotalR(data[data.length -1 ].total_recovered));
   }
   // getTotal()
   setInterval(getTotal(), getTotalD(), getTotalR(), 3600000);
   return (
     <div className="total-card">
-      <span>{`ผู้ป่วยสะสม : ` + totalData.totalDataNew}</span>
-      <span>{`เสียชีวิต : ` + totalDataD.totalDataDet}</span>
-      <span>{`หายกลับบ้าน : ` + totalDataR.totalDataRec}</span>
+      <span>{`ผู้ป่วยสะสม : ` + totalData}</span>
+      <span>{`เสียชีวิต : ` + totalDataD}</span>
+      <span>{`หายกลับบ้าน : ` + totalDataR}</span>
     </div>
   );
 };
